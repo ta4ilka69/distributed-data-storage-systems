f8bcb84253e228b558e35101ca2a6d631ba75863
ta4ilka69 <artem_balin_2004@mail.ru>
2025-03-20 14:01:20 +0300

    some front to work

--- ZOV/zapadniy-front/vite.config.ts
+++ ZOV/zapadniy-front/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window',
+  },
+  resolve: {
+    alias: {
+      'socket.io-client': 'socket.io-client/dist/socket.io.js',
+    }
+  }
 })
